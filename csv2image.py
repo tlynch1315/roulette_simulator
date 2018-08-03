c021f4e20c3ab67e8f749677919538de3e0468e5
Pat Link <patricklink05@gmail.com>
2018-08-02 23:29:20 -0400

    about to make new branch

--- csv2image.py
+++ csv2image.py
@@ -39,6 +39,37 @@ def pandas2image(axes, value,df, limiters=None):
                 "x_name": xaxis_name,
                 "y_name": yaxis_name}
     elif len(axes) == 3:
+        xaxis_name = axes[0]
+        yaxis_name = axes[1]
+        zaxis_name = axes[2]
+        xaxis = df[xaxis_name].unique()
+        yaxis = df[yaxis_name].unique()
+        zaxis = df[zaxis_name].unique()
+        pixel_array = np.zeros([len(xaxis), len(yaxis), len(zaxis)])
+
+        #print np.shape(pixel_array)
+        df = df.set_index([xaxis_name, yaxis_name, zaxis_name])
+        #print df
+        for i1, x in enumerate(xaxis):
+            for i2, y in enumerate(yaxis):
+                for i3, z in enumerate(zaxis):
+                    try:
+                        val = df.loc[x,y,z][value]
+                        try:
+                            pixel_array[i1][i2] = val
+                        except ValueError:
+                            pixel_array[i1][i2] = val.mean()
+                    except KeyError:
+                        val = np.nan
+                        pixel_array[i1][i2] = val
+
+        return {"data" : pixel_array,
+                "xaxis": xaxis,
+                "yaxis": yaxis,
+                "zaxis": zaxis,
+                "x_name": xaxis_name,
+                "y_name": yaxis_name,
+                "z_name": zaxis_name}
         print "not ready yet"
 
 
